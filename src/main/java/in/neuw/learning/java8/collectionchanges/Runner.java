397aa96899c68914dcd74b8db3f3d0802f954695
Karanbir Singh <krnbr@live.in>
2018-12-31 23:28:30 +0530

    minor changes

--- src/main/java/in/neuw/learning/java8/collectionchanges/Runner.java
+++ src/main/java/in/neuw/learning/java8/collectionchanges/Runner.java
@@ -2,6 +2,7 @@ package in.neuw.learning.java8.collectionchanges;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static net.andreinc.mockneat.unit.objects.Filler.filler;
 import static net.andreinc.mockneat.unit.types.Ints.ints;
@@ -13,39 +14,39 @@ import static net.andreinc.mockneat.unit.user.Users.users;
  **/
 public class Runner {
 
-    public static void mockData(){
+    public static List<Person> mockData(){
         List<Person> p = filler(() -> new Person())
-                //.setter(Person::setAge, )
                 .setter(Person::setId, ints().bound(100000))
                 .setter(Person::setName, names().full())
                 .setter(Person::setAge, ints().range(1,110))
                 .list(() -> new ArrayList<>(), 10) // Collecting all the results ina  List of 10 elements.
                 .get();
-        System.out.println(p);
+        //System.out.println(p);
+        return p;
     }
 
     public static void main(String[] args) {
 
-        List<Person> persons = new ArrayList<Person>();
+        List<Person> persons = mockData();
 
-        persons.add(new Person(1,"Karanbir Singh",29));
+        System.out.println(persons);
+
+        /*persons.add(new Person(1,"Karanbir Singh",29));
         persons.add(new Person(2,"Surinder Singh",56));
         persons.add(new Person(3,"Rajnit Kaur",53));
         persons.add(new Person(4,"Gurkirat Kaur",29));
         persons.add(new Person(5,"Harmeet Singh",30));
         persons.add(new Person(6,"Kiratleen Kaur",25));
-        persons.add(new Person(7,"Kainaaz Kaur",1));
+        persons.add(new Person(7,"Kainaaz Kaur",1));*/
 
         //persons.stream().forEachOrdered(System.out::println);
 
-        persons.stream().filter(person -> {
+        List<Person> filteredPersons = persons.stream().filter(person -> {
             System.out.println(person);
-            return person.getId()<=3;
-        });
+            return person.getAge()<=20;
+        }).collect(Collectors.toList());
 
-        System.out.println(persons);
-
-        mockData();
+        System.out.println(filteredPersons);
 
     }
 
