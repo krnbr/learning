90991214bd7fdf69abcc7e005583b6d038f4ef9f
Karanbir Singh <krnbr@live.in>
2019-06-20 12:03:10 +0530

    mouse disco and pom.xml improvements

---
+++ src/main/java/in/n2w/MouseDisco.java
@@ -0,0 +1,23 @@
+package in.n2w;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Created by Karanbir Singh on 6/20/2019.
+ **/
+public class MouseDisco {
+
+    public static void main(String[] args) throws AWTException {
+        Robot hal = new Robot();
+        Random random = new Random();
+        while (true) {
+            hal.delay(1000 * 10);
+            int x = random.nextInt(1366);
+            int y = random.nextInt(768);
+            System.out.println("(x,y):(" + x + "," + y + ")");
+            hal.mouseMove(x, y);
+        }
+    }
+
+}
